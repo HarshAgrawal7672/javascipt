0165198d9b2464690886ef67af315aaa201fa9c6
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-02-03 17:14:27 +0530

    learning about variables

---
+++ variable.js
@@ -0,0 +1,9 @@
+const id=12345
+var name="harsh"//this is not preffered becuase issue in block scope and functional scope
+let state="rajesthan"
+city="jaipur"// this is also not preffered
+let fav;
+
+console.log(id, name, state, city,fav);
+console.table([id, name, state, city,fav]);
+
