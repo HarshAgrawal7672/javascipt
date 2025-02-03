8a6ea34df3fd2fed65c052f239c853c4a696505f
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-02-03 17:39:11 +0530

    learning about datatypes

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treats code as newer versions
+
+// alert(3+3); //we ae using node not browser
+
+//  we should improve code readability
+
+let name="harsh"
+let age=19
+let isloggedin=false
+
+// number=> 2 to power 53
+// bigint
+// string==> '' or ""
+// boolean==> true or false
+// null ==> standalone value
+// undefined
+// symbol==> unique value
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+// null is an object
+
