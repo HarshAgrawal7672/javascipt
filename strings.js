aa8726db5be57f1115086748283cbf1882787c16
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-02-04 12:58:33 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,22 @@
+const name="harsh"
+const repo=20
+
+console.log(name+repo+"vale");
+
+console.log(`name: ${name} repo: ${repo}`);//new way
+
+const gamename=new String("      harsh agrawal      ") // as a object 
+
+console.log(typeof gamename);
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+
+console.log(gamename.toUpperCase());
+console.log(gamename.trim());
+
+
+
+
+
