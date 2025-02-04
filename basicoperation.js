32499d7b4ee9316899c1c9dd6325ac0bf2754b96
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-02-04 10:28:42 +0530

    basic operations

---
+++ basicoperation.js
@@ -0,0 +1,32 @@
+let value=3
+let nengvalue=-value
+
+// console.log(nengvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="hello"
+let str2=" world"
+let str3=str1+str2
+
+console.log(str3);
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true+2+"3");
+console.log(+"");
+
+
+
+
+
+
+
+
+
+
+
+
