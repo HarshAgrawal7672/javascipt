1ec1606f7e5d6524d92f5dcf0ad4c688f4dd37d6
HarshAgrawal7672 <harshagrawal7895@gmail.com>
2025-02-04 12:39:26 +0530

    summary of datatypes and stack and heap basic

---
+++ datatypessummary.js
@@ -0,0 +1,51 @@
+//  primitive
+
+// 7 types: string,number,boolean,null,undefined,symbol,BigInt
+// non primitive
+
+// dynamiclly type  
+
+const id= Symbol('123')
+const anotherid= Symbol('123')
+
+console.log(id, anotherid);
+console.log(id===anotherid);
+
+const bignumber=146494646646879749879n
+console.log(typeof bignumber);
+
+
+
+
+
+// reference types
+// array,Object,Function
+
+const arr=[1,2,3,4,5,6,7,8,9]
+
+let myobj={
+    name:"harsh",
+    age:19,
+}
+
+ function myfunction(){
+    x=2;
+    y=3;
+}
+
+
+// stack( primitives),
+// heap(nonprimitives)
+
+
+
+
+
+
+
+
+
+
+
+
+
